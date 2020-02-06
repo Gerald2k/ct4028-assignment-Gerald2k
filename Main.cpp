60071bbfe797daf383cf2cff1f34433ffd843f1b
Gerald2k <55755923+Gerald2k@users.noreply.github.com>
2020-02-06 16:30:33 +0000

    Initial files

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+int main(int argc, char* argv[])
+{
+	UNREFERENCED_PARAMETER(argc);
+	UNREFERENCED_PARAMETER(argv);
+
+	return 0;
+}
+
---
+++ rc_pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// System includes
+#include <Windows.h>
+
+// STL includes
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
